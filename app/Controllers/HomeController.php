0c41c6a3eb7b8a39ed496a66cd05be77703a36a0
Diego Fernandes <diegofernandes@infojr.com.br>
2018-02-13 15:16:16 -0300

    Cria a primeira view (aula 13)

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,8 +4,16 @@ namespace App\Controllers;
 
 class HomeController
 {
+	private $view;
+
+	public function __construct()
+	{
+		$this->view = new \stdClass;
+	}
+
 	public function index(){
-		echo "Home";
+		$this->view->nome = "Diego Fernandes";
+		require_once __DIR__ . "/../Views/home/index.phtml";
 
 	}
 }
